5ddc3606e5b1a16cccba752c3f9f01d36c6e4472
Vivekjyoti Banerjee <vivekjyoti24@gmail.com>
2025-06-18 22:10:35 -0400

    reword, add extra fields in rsvp

--- js/form_submission.js
+++ js/form_submission.js
@@ -5,8 +5,10 @@ form.addEventListener('submit', async (event) => {
   event.preventDefault();
 
   const formData = {
-    name: document.getElementById('name').value,
+    first_name: document.getElementById('first-name').value,
+    last_name: document.getElementById('last-name').value,
     email: document.getElementById('email').value,
+    phone_number: document.getElementById('phone-number').value,
     num_guests: document.getElementById('num-guests').value,
     diet: document.getElementById('diet').value,
     need_parking: document.getElementById('need-parking').checked,
@@ -14,7 +16,7 @@ form.addEventListener('submit', async (event) => {
   };
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbycKu2hAg5-0ecY9Lgwp7c67AfovQ2qSTkH8pC_7mct4uIwrMzjqKa5YOa3T_NuEJs/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbwZLKkCNTEAcInCkgUJTuClKabezSFq-1dxlmp4KSSeUDLIAII4CExhHZQn7fa0jv-l/exec', {
       method: 'POST',
       body: JSON.stringify(formData)
     });
